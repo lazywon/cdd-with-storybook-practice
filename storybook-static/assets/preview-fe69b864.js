b16fd50e9e3d82934ea616a96210a2c40f887d98
lazywon <jinjooisme@gmail.com>
2023-06-17 11:42:36 +0900

    deploy storybook

---
+++ storybook-static/assets/preview-fe69b864.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-7f5b5f8d.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-915cee48.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-915cee48.js","./iframe-7f5b5f8d.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-63c5d83d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-fe69b864.js.map
